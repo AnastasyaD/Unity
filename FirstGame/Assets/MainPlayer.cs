35cb3010c329ddda6d13917b57a4ec301ae25f41
AnastasyaD <davybead@gmail.com>
2020-11-23 02:16:51 +0300

    Test

---
+++ FirstGame/Assets/MainPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+   
+    private float _speed = 3;
+    private float _turnSpeed = 15;
+    private Vector3 _direction = Vector3.zero;
+
+    private void Update()
+    {
+        _direction.x = -Input.GetAxis("Horizontal");
+        _direction.z = -Input.GetAxis("Vertical");
+        
+        transform.position += _direction * _speed * Time.deltaTime;
+        
+    }
+}
---
+++ FirstGame/Assets/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+   
+    
+      private float _speed = 15;
+    private Vector3 _direction = Vector3.zero;
+
+    private void Update()
+    {
+        _direction.x = -Input.GetAxis("Horizontal");
+        _direction.z = -Input.GetAxis("Vertical");
+
+        transform.position += _direction * Time.deltaTime;
+    }
+}
