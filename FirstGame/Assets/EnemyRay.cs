19e53a0d9d732dffcaf0f4da9c1bb4231bf99bc4
AnastasyaD <davybead@gmail.com>
2020-12-06 01:20:44 +0300

    Mesh, parts of enemy's code ect

---
+++ FirstGame/Assets/EnemyRay.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyRay : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent navMeshAgent;
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private Transform _player;
+    [SerializeField] private LayerMask _mask;
+    private RaycastHit hit;
+    private float _startOffset = 1.5f;
+
+    private bool isPatrol = true;    
+    private int m_CurrentWaypointIndex = 0;
+   
+
+    private void Awake(){
+       _player = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+    private void Start() 
+    {
+        navMeshAgent.SetDestination (waypoints[m_CurrentWaypointIndex].position);
+        navMeshAgent.speed = Random.Range(0.5f, 2f);
+    }
+
+    private void FixedUpdate() 
+    {
+        var color = Color.red;
+        RaycastHit hit;
+        
+        var startRaycastPosition = CalculateOffset(transform.position);
+        var directionToPlayer = CalculateOffset(_player.position) - startRaycastPosition;
+
+        var rayCast = Physics.Raycast(startRaycastPosition, directionToPlayer, out hit, directionToPlayer.magnitude, _mask);
+
+        if (rayCast)
+        {
+               if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                color = Color.green;
+                FollowTarget();
+             }
+             else
+            {
+                Comeback();
+            }
+
+        }
+
+        Debug.DrawRay(startRaycastPosition, directionToPlayer, color);
+    }
+
+    public void SetWaypoint(Transform[] wayPoints)
+    {
+        waypoints = wayPoints;
+    }
+    private Vector3 CalculateOffset(Vector3 position)
+    {
+        position.y += _startOffset;
+        return position;
+    }
+
+    private void Comeback()
+    {
+        if (isPatrol == true && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {            
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % waypoints.Length;
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+        }
+        else if (!isPatrol)
+        {
+            Wait();
+            Invoke(nameof(Go), 2f);
+            navMeshAgent.SetDestination(waypoints[m_CurrentWaypointIndex].position);
+            isPatrol = true;
+        }
+    }
+    private void FollowTarget()
+    {
+        isPatrol = false;
+        navMeshAgent.SetDestination(_player.position);
+    }
+
+    private void Wait()
+    {
+        navMeshAgent.isStopped = true;
+    }
+
+    private void Go()
+    {
+        navMeshAgent.isStopped = false;
+    }
+
+}
---
+++ FirstGame/Assets/SpawnPoint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject[] spawnPoint;
+    public GameObject enemy;
+
+    private void Awake() {
+      spawnPoint = GameObject.FindGameObjectsWithTag("Spawn");  
+    }
+    private void Start() {
+        for (int i=0; i<spawnPoint.Length; i++)
+        {
+            Instantiate(enemy, spawnPoint[i].transform.position, spawnPoint[i].transform.rotation, spawnPoint[i].transform);
+        }
+        
+    }
+}
